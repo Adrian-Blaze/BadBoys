00c23dfdf3f0b2a0b810095e107d5d011b81bcdd
Adrian-Blaze <Adrian-Blaze@github.com>
2024-10-08 22:30:58 +0100

    optimize model to get needed data directly from DB

--- pages/🔮BBHS_model.py
+++ pages/🔮BBHS_model.py
@@ -1,13 +1,89 @@
 import pickle
 import streamlit as st
 import numpy as np
+import  psycopg2
+
+def initialize_DB_connection():
+   # Connection string
+  conn_str = 'postgresql://postgres:tFKz6F7xrxEjHCC9@fixedly-distinct-jackrabbit.data-1.use1.tembo.io:5432/postgres'
+
+  try:
+     global conn
+      # Create a new database session
+     conn = psycopg2.connect(conn_str)
+  except Exception as e:
+      print(f"Unable to connect to the database: {e}")
+   
+
+def get_mock_score():
+   initialize_DB_connection()
+   try:
+        # Create a new cursor object.
+        cur = conn.cursor()
+        query = """
+        Select performance 
+        from performance
+        where exam_type = 'mock'
+        """
+
+        # Test Query Select performance from performance  where exam_type = 'mock'
+        cur.execute(query)
+        global output
+
+        # Fetch result
+        output = cur.fetchall()
+        # Extract values from the tuples
+        values = [x[0] for x in output]
+
+        # Calculate the average
+        average = sum(values) / len(values)
+        return(average)
+   except Exception as e:
+        print(f"An error occurred: {e}")
 
 
-st.title('BBHS student prediction model for external exams 🔮')
+def get_term_score():
+   initialize_DB_connection()
+   try:
+        # Create a new cursor object.
+        cur = conn.cursor()
+        query = """
+        Select performance 
+        from performance
+        where exam_type = 'previous-term'
+        """
 
-Study_hours = st.text_input("Enter weekly study hours", max_chars=2)
-prev_grade = st.text_input("Enter cummulative from previous term percentage", max_chars=3) 
-mock_grade = st.text_input("Enter mock cummulative percentage", max_chars=3)  
+        # Test Query Select performance from performance  where exam_type = 'mock'
+        cur.execute(query)
+        global output
+
+        # Fetch result
+        output = cur.fetchall()
+        
+        return(output[0][0])
+   except Exception as e:
+        print(f"An error occurred: {e}")
+
+
+def get_study_hours():
+    initialize_DB_connection()
+    try:
+        # Create a new cursor object.
+        cur = conn.cursor()
+        query = """
+        SELECT study_hours 
+        FROM students
+        where username = 'BBHS-1002'
+        """
+        # Test Query Select performance from performance  where exam_type = 'mock'
+        cur.execute(query)
+        global output
+
+        # Fetch result
+        output = cur.fetchall()
+        return(output[0][0])
+    except Exception as e:
+        print(f"An error occurred: {e}")
 
 
 def predict_price():
@@ -25,4 +101,14 @@ def predict_price():
       st.error('Likely to fail, Keep pushing harder') 
 
 
-st.button('Predict', on_click=predict_price)
+
+st.title('BBHS student prediction model for external exams 🔮')
+predict = st.button('Predict')
+if predict:
+    Study_hours = get_study_hours()
+    st.write(f'Study hours as recorded in database is, {Study_hours} hours')
+    mock_grade = get_mock_score()
+    st.write(f'Mock Cummulative as recorded in database is, {mock_grade}%')
+    prev_grade = get_term_score()
+    st.write(f'Last Term Cummulative as recorded in database is, {prev_grade}%')
+    predict_price()
